ef6b8bf32eb227ed77ecf3092edff98e46ae348b
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2025-05-17 13:14:25 -0500

    Add files via upload

---
+++ proyectoBancoUQ/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.uniquindio.edu.co.poo.proyectobancouq {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens org.uniquindio.edu.co.poo.proyectobancouq to javafx.fxml;
+    exports org.uniquindio.edu.co.poo.proyectobancouq;
+}
