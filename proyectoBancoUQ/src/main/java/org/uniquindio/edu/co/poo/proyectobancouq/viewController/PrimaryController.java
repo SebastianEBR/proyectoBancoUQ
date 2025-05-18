17da7105cc0b07487540444c05f53830d6a8da86
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2025-05-18 18:54:04 -0500

    un poco de la clase App

---
+++ proyectoBancoUQ/src/main/java/org/uniquindio/edu/co/poo/proyectobancouq/viewController/PrimaryController.java
@@ -0,0 +1,16 @@
+package org.uniquindio.edu.co.poo.proyectobancouq.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PrimaryController {
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    private void onOpenUsuario() {
+        System.out.println("Abriendo la gestión de banco...");
+        // Aquí puedes agregar la lógica para cambiar de vista
+    }
+}
