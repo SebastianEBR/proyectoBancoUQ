ae53c79a4bcd143c9f600cf04154495c623b91ac
JuanLasso-Lint <Juane.lassob@uqvirtual.edu.co>
2025-05-25 01:06:17 -0500

    se corrigen errores

--- proyectoBancoUQ/src/main/java/org/uniquindio/edu/co/poo/proyectobancouq/viewController/ListaClientesReporte.java
+++ proyectoBancoUQ/src/main/java/org/uniquindio/edu/co/poo/proyectobancouq/viewController/ListaClientesReporte.java
@@ -26,9 +26,6 @@ public class ListaClientesReporte {
     private AnchorPane ReportePane;
 
     @FXML
-    private AnchorPane ClientesPane; // Nuevo: para el panel de clientes
-
-    @FXML
     private ComboBox<?> SelcTipoCuenta;
 
     @FXML
@@ -67,52 +64,39 @@ public class ListaClientesReporte {
     @FXML
     private TextField txtSaldoInicialCuenta;
 
-    // MÉTODOS DE NAVEGACIÓN ENTRE PANELES
-
     @FXML
     void BuscarUsuario(ActionEvent event) {
-        ClientesPane.setVisible(true);
-        ReportePane.setVisible(false);
-        PaneNuevaCUenta.setVisible(false);
-    }
 
-    @FXML
-    void mostrarReporte(ActionEvent event) {
-        ClientesPane.setVisible(false);
-        ReportePane.setVisible(true);
-        PaneNuevaCUenta.setVisible(false);
     }
 
     @FXML
     void mostrarNuevaCuenta(ActionEvent event) {
-        ClientesPane.setVisible(false);
-        ReportePane.setVisible(false);
-        PaneNuevaCUenta.setVisible(true);
+
     }
 
-    // MÉTODO DE INICIALIZACIÓN
+    @FXML
+    void mostrarReporte(ActionEvent event) {
+
+    }
+
     @FXML
     void initialize() {
-        assert ClientesPane != null : "fx:id=\"ClientesPane\" was not injected: check your FXML file.";
-        assert PaneNuevaCUenta != null : "fx:id=\"PaneNuevaCUenta\" was not injected: check your FXML file.";
-        assert ReportePane != null : "fx:id=\"ReportePane\" was not injected: check your FXML file.";
-        assert SelcTipoCuenta != null : "fx:id=\"SelcTipoCuenta\" was not injected: check your FXML file.";
-        assert btnBuscar != null : "fx:id=\"btnBuscar\" was not injected: check your FXML file.";
-        assert btnMostrarReporte != null : "fx:id=\"btnMostrarReporte\" was not injected: check your FXML file.";
-        assert btnNuevaCuenta != null : "fx:id=\"btnNuevaCuenta\" was not injected: check your FXML file.";
-        assert colCorreo != null : "fx:id=\"colCorreo\" was not injected: check your FXML file.";
-        assert colFechaTransaccion != null : "fx:id=\"colFechaTransaccion\" was not injected: check your FXML file.";
-        assert colID != null : "fx:id=\"colID\" was not injected: check your FXML file.";
-        assert colNombres != null : "fx:id=\"colNombres\" was not injected: check your FXML file.";
-        assert colTipoTransaccion != null : "fx:id=\"colTipoTransaccion\" was not injected: check your FXML file.";
-        assert tableClientes != null : "fx:id=\"tableClientes\" was not injected: check your FXML file.";
-        assert tableReporte != null : "fx:id=\"tableReporte\" was not injected: check your FXML file.";
-        assert txtNuevoNumCuenta != null : "fx:id=\"txtNuevoNumCuenta\" was not injected: check your FXML file.";
-        assert txtSaldoInicialCuenta != null : "fx:id=\"txtSaldoInicialCuenta\" was not injected: check your FXML file.";
+        assert PaneNuevaCUenta != null : "fx:id=\"PaneNuevaCUenta\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert ReportePane != null : "fx:id=\"ReportePane\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert SelcTipoCuenta != null : "fx:id=\"SelcTipoCuenta\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert btnBuscar != null : "fx:id=\"btnBuscar\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert btnMostrarReporte != null : "fx:id=\"btnMostrarReporte\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert btnNuevaCuenta != null : "fx:id=\"btnNuevaCuenta\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert colCorreo != null : "fx:id=\"colCorreo\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert colFechaTransaccion != null : "fx:id=\"colFechaTransaccion\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert colID != null : "fx:id=\"colID\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert colNombres != null : "fx:id=\"colNombres\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert colTipoTransaccion != null : "fx:id=\"colTipoTransaccion\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert tableClientes != null : "fx:id=\"tableClientes\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert tableReporte != null : "fx:id=\"tableReporte\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert txtNuevoNumCuenta != null : "fx:id=\"txtNuevoNumCuenta\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
+        assert txtSaldoInicialCuenta != null : "fx:id=\"txtSaldoInicialCuenta\" was not injected: check your FXML file 'ListaClientesReportes.fxml'.";
 
-        // Mostrar solo el panel de clientes al inicio
-        ClientesPane.setVisible(true);
-        ReportePane.setVisible(false);
-        PaneNuevaCUenta.setVisible(false);
     }
+
 }
